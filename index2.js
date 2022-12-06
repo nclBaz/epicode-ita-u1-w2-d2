d404ac246b40777d36d6f9b6913f84ce4a48434d
Riccardo Gulin <riccardo.gulin@gmail.com>
2022-12-06 10:52:18 +0100

    Adds objects examples

--- index2.js
+++ index2.js
@@ -7,7 +7,7 @@
 // Gli oggetti sono definiti da: parentesi graffe che racchiudono un elenco di PROPRIETA' (coppia chiave:valore)
 
 const studente = {
-  id: 1,
+  id: 1, // Proprietà 0 --> NUMBER
   nome: "Riccardo", // Proprietà 1 --> STRING
   cognome: "Gulin", // Proprietà 2 --> STRING
   età: 18, // Proprietà 3 --> NUMBER
@@ -27,14 +27,52 @@ const pet = {
   specie: "Gatto",
   colore: "Rosso",
   anni: 10,
+  "ha pedegree": true,
 }
 
-console.log(studente)
+/* console.log(studente)
 
 // COME POSSO ACCEDERE AD UNA SINGOLA PROPRIETA' DELL'OGGETTO (IN LETTURA E IN SCRITTURA)??
 
 console.log("L'età del primo studente è: ", studente.età)
+console.log("L'età del primo studente è: ", studente["età"]) // ALTERNATIVA ALLA DOT NOTATION (MEGLIO LA DOT NOTATION)
 console.log("Il nome del primo studente è: ", studente.nome)
 
 studente.età = 30 // SOVRASCRIVO L'ETA' DEL PRIMO STUDENTE
 console.log("L'età del primo studente è: ", studente.età)
+
+console.log(pet["ha pedegree"]) // QUANDO IL NOME PROPRIETA' CONTIENE UNO SPAZIO NON POSSO USARE LA DOT NOTATION
+
+// CANCELLARE PROPRIETA' DALL'OGGETTO
+
+delete studente.id // Elimina la proprietà id dallo studente
+console.log(studente.id) // studente.id ora sarà undefined
+ */
+
+// COPIARE OGGETTI
+
+/* const pet2 = pet // Non è la maniera giusta per copiare le proprietà da un oggetto ad un altro
+
+console.log("Pet2: ", pet2)
+
+pet.nome = "Pedro"
+
+pet2.occhi = 3
+
+console.log("Pet: ", pet)
+console.log("Pet2: ", pet2) */
+
+/* const pet2 = {}
+
+Object.assign(pet2, pet) // COPIARE PROPRIETA' DA pet A pet2 IN MANIERA CORRETTA
+// ALTERNATIVA A OBJECT.ASSIGN --> const pet2 = {...pet}
+
+console.log("Pet2: ", pet2)
+
+pet.nome = "Pedro"
+
+pet2.occhi = 3
+
+console.log("Pet: ", pet)
+console.log("Pet2: ", pet2)
+ */
